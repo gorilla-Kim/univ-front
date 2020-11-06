96446556bac83445e9a7810c668e1405e6960626
BY <devblossom77@gmail.com>
2020-11-07 02:35:42 +0900

    헤더 - 메뉴데이터 추가

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -4,12 +4,126 @@ import {Layout as AntdLayout} from 'antd';
 import Images from '../../assets/image'
 import Menu from "../Menu/Menu";
 
+const navigatorList = [
+  {
+    icon: '',
+    title: 'Introduction & Research',
+    key: 'Introduction & Research',
+    subMenu: [
+      {
+        icon: '',
+        title: 'Introduction',
+        key: '/introduction',
+        subMenu: []
+      },
+      {
+        icon: '',
+        title: 'Research',
+        key: '/introduction/research',
+        subMenu: []
+      }
+    ]
+  },
+  //2
+  {
+    icon: '',
+    title: 'Professor',
+    key: 'Professor',
+    subMenu: [
+      {
+        icon: '',
+        title: 'Professor',
+        key: '/professor',
+        subMenu: []
+      },
+    ]
+  },
+  //3
+  {
+    icon: '',
+    title: 'Member',
+    key: 'Member',
+    subMenu: [
+      {
+        icon: '',
+        title: 'Member',
+        key: '/member/current',
+        subMenu: []
+      },
+      {
+        icon: '',
+        title: 'Alumni',
+        key: '/member/alumni',
+        subMenu: []
+      }
+    ]
+  },
+  //4
+  {
+    icon: '',
+    title: 'Publications',
+    key: '/publications',
+    subMenu: []
+  },
+  //5
+  {
+    icon: '',
+    title: 'Lecture',
+    key: 'Lecture',
+    subMenu: [
+      {
+        icon: '',
+        title: 'A',
+        key: '/lecture/A',
+        subMenu: []
+      },
+      {
+        icon: '',
+        title: 'B',
+        key: '/lecture/B',
+        subMenu: []
+      },
+      {
+        icon: '',
+        title: 'C',
+        key: '/lecture/C',
+        subMenu: []
+      },
+    ]
+  },
+  //6
+  {
+    icon: '',
+    title: 'Community',
+    key: 'Community',
+    subMenu: [
+      {
+        icon: '',
+        title: 'Notice',
+        key: '/community/notice',
+        subMenu: []
+      },
+      {
+        icon: '',
+        title: 'Gallery',
+        key: '/community/gallery',
+        subMenu: []
+      },
+      {
+        icon: '',
+        title: 'Board',
+        key: '/community/board',
+        subMenu: []
+      },
+    ]
+  },
+]
+
 const Header = (props) => {
 
   return (
     <Wrapper>
-      <Menu/>
-
+      <Menu list={navigatorList} mode={'horizontal'}/>
     </Wrapper>
   )
 };
