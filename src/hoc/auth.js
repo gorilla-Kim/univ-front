0260c125c50269ef4f582e9078e9f7b202ce0ae6
BY <devblossom77@gmail.com>
2020-11-08 19:14:58 +0900

    무한루프 유발로 App 라우터에서 [Auth HOC] 제거 / 리덕스툴 설치

--- src/hoc/auth.js
+++ src/hoc/auth.js
@@ -8,18 +8,17 @@ export default (Component, option, adminRoute = null) => {
                    false -> 로그인한 유저는 출입이 불가능한 페이지
     */
     const AuthenticateCheck = (props) => {
-        const isLoggedIn = useSelector(state => state.user.isLoggedIn);
-
-        useEffect(() => {
-            if (!isLoggedIn && option) { // 로그인 안됐고, 로그인 유저만 출입 가능한 페이지면
-                props.history.push('/login'); // 로그인으로
-            }
-        }, []);
+        // const isLoggedIn = useSelector(state => state.user.isLoggedIn);
+        //
+        // useEffect(() => {
+        //     if (!isLoggedIn && option) { // 로그인 안됐고, 로그인 유저만 출입 가능한 페이지면
+        //         props.history.push('/login'); // 로그인으로
+        //     }
+        // }, []);
 
         return (
             <Component/>
         )
-
     };
 
     return AuthenticateCheck;
